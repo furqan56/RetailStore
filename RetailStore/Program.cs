e3de70b17bb0bbcfd929e745f70d2acbe43e2d08
Furqan Khan <furqankhan5566@gmail.com>
2019-07-05 18:47:34 +0500

    little code change

--- RetailStore/Program.cs
+++ RetailStore/Program.cs
@@ -17,8 +17,18 @@ namespace RetailStore
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddCommandLine(args)
+                .Build();
+
+            return WebHost.CreateDefaultBuilder(args)
+                .UseConfiguration(config)
                 .UseStartup<Startup>();
+        }
+            
     }
 }
--- RetailStore/Startup.cs
+++ RetailStore/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -20,6 +21,7 @@ namespace RetailStore
     {
         public Startup(IConfiguration configuration)
         {
+           
             Configuration = configuration;
         }
 
